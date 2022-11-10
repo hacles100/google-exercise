cb28b73115e388622ddafa78830cbdb29dc8d438
Abel Mendonça <abel.ncm@gmail.com>
2022-11-10 11:06:53 -0100

    map reactjs

--- src/pages/all/All.js
+++ src/pages/all/All.js
@@ -3,20 +3,37 @@ import Result from "../../components/result/Result";
 
 function All() {
 
+    const list = [
+        {
+            url: 'https://academy.com',
+            title: 'This is Academy!',
+            summary: 'Academy is a webpage that does this and that and etc. Academy is a webpage that does this and that and etc',
+        },
+        {
+            url: 'https://praia-shopping.com',
+            title: 'Cinema na Praia Shopping',
+            summary: 'Pão quente não tem pão quente, apenas pão frio. Academy is a webpage that does this and that and etc',
+        },
+        {
+            url: 'https://somada.com',
+            title: 'Somada',
+            summary: 'Somas não tem pão quente, apenas pão frio. Academy is a webpage that does this and that and etc',
+        },
+        {
+            url: 'https://somada.com',
+            title: 'Somada',
+            summary: 'Somas não tem pão quente, apenas pão frio. Academy is a webpage that does this and that and etc',
+        }
+    ]
+
     return <>
         <h1>All page</h1>
-        
-        <Result
-            url='https://facebook.com'
-            title='This is Meta!'
-            summary='Face is a webpage that does this and that and etc. 
-                Face is a webpage that does this and that and etc' />
 
-        <Result
-            url='https://facebook.com'
-            title='This is Meta!'
-            summary='Face is a webpage that does this and that and etc. 
-                Face is a webpage that does this and that and etc' />
+        {list.map((item, i)=><Result
+            key={i}
+            url={item.url}
+            title={item.title}
+            summary={item.summary} />)}
     </>
 
 }
